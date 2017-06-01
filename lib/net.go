c9795d21c7f1f2d8853b3de1ee37813a776f4abc
aerth <aerth@riseup.net>
2017-06-01 04:46:08 +0000

    fix auth

--- lib/net.go
+++ lib/net.go
@@ -179,7 +179,7 @@ func (c *Connection) readerwriter() error {
 		case "NOTICE":
 			if irc.ReplyTo == "NickServ" {
 				//				:NickServ!NickServ@services. NOTICE aerth :mustangsally ACC 3
-				if irc.Message == fmt.Sprintf("%s ACC 3", c.config.Master) {
+				if irc.Raw == fmt.Sprintf("%s ACC 3 ", strings.Split(c.config.Master, ":")[0]) {
 					c.masterauth = time.Now()
 
 				}
