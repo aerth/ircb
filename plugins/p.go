e0a86d5e7e1b1052b71725dcefa26d8a2ef5d31a
aerth <aerth@riseup.net>
2017-06-03 03:57:12 +0000

    testing new thing

--- plugins/p.go
+++ plugins/p.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"os"
+	"strconv"
 	"time"
 
 	ircb "github.com/aerth/ircb/lib/ircb"
 )
 
-func main() {
-	println("plugin main")
-}
 func Init(c map[string]ircb.Command, m map[string]ircb.Command) (map[string]ircb.Command, map[string]ircb.Command, error) {
 	if c == nil {
 		c = make(map[string]ircb.Command)
@@ -25,6 +25,34 @@ func Init(c map[string]ircb.Command, m map[string]ircb.Command) (map[string]ircb
 
 func CommandTime(c *ircb.Connection, irc *ircb.IRC) {
 	irc.Reply(c, time.Now().String())
+}
+func CommandSeen(c *ircb.Connection, irc *ircb.IRC) {
+	if len(irc.Arguments) == 0 {
+		stat, err := os.Stat(".log.txt")
+		if err == nil {
+			irc.Reply(c, strconv.FormatUint(uint64(stat.Size), 10))
+		}
+		return
+	}
+
+	if len(irc.Arguments) != 1 {
+		return
+	}
+
+	f, err := os.Open(".log.txt")
+	if err != nil {
+		c.SendMaster("%q", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parsed := ircb.Parse(line)
+		if parsed.ReplyTo == irc.Arguments[0] {
+			irc.ReplyUser(c, parsed.Message)
+		}
+	}
 }
 
 func MasterCommandReloadPlugin(c *ircb.Connection, irc *ircb.IRC) {
