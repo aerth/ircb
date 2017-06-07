92e794465f294ceaf7bd00d1252348dfc35e606b
aerth <aerth@riseup.net>
2017-06-07 23:30:05 +0000

    remove karma.go

--- commands.go
+++ commands.go
@@ -354,3 +354,42 @@ func masterCommandFetchPlugin(c *Connection, irc *IRC) {
 	irc.Reply(c, fmt.Sprintf(Green+"plugin loaded: %q", name))
 
 }
+
+func commandKarma(c *Connection, irc *IRC) {
+	if len(irc.Arguments) != 1 {
+		irc.Reply(c, c.karmaShow(irc.ReplyTo))
+		return
+	}
+
+	irc.Reply(c, c.karmaShow(irc.Arguments[0]))
+}
+func (c *Connection) parseKarma(input string) (handled bool) {
+	handled = false
+	split := strings.Split(input, " ")
+	if len(split) < 1 {
+		return false
+	}
+
+	if len(split) > 1 {
+		if strings.Contains(input, "thank") {
+			if i := strings.Index(input, ":"); i != -1 && i != 0 {
+				c.Log.Println("Karma:", input[0:i])
+				c.karmaUp(input[0:i])
+				return true
+			}
+			return false
+		}
+		return false
+	}
+
+	if strings.HasSuffix(input, "+") {
+		c.karmaUp(strings.Replace(input, "+", "", -1))
+		return true
+	}
+
+	if strings.HasSuffix(input, "-") {
+		c.karmaDown(strings.Replace(input, "-", "", -1))
+		return true
+	}
+	return false
+}
